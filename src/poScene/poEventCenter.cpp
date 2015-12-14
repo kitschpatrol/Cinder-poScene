cad7ec99654c34878fba0a580c509cb61c5657f4
Eric Mika <eric@ericmika.com>
2015-12-14 14:40:27 -0500

    Formatting.

--- src/poScene/poEventCenter.cpp
+++ src/poScene/poEventCenter.cpp
@@ -1,21 +1,21 @@
 /*
  Copyright (c) 2015, Potion Design LLC
  All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
- 
+
  * Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
- 
+
  * Redistributions in binary form must reproduce the above copyright notice,
  this list of conditions and the following disclaimer in the documentation
  and/or other materials provided with the distribution.
- 
+
  * Neither the name of copyright holder nor the names of its
  contributors may be used to endorse or promote products derived from
  this software without specific prior written permission.
- 
+
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -32,41 +32,35 @@
 #include "poEventCenter.h"
 #include "poScene.h"
 
-namespace po { namespace scene {
+namespace po {
+namespace scene {
 
-	EventCenterRef EventCenter::create()
-	{
-		return EventCenterRef(new EventCenter());
-	}
-	
-	EventCenter::EventCenter()
-	{
-		//	Connect mouse events
-        ci::app::getWindow()->getSignalMouseDown().connect(std::bind(&EventCenter::mouseDown, this, std::placeholders::_1));
-        ci::app::getWindow()->getSignalMouseMove().connect(std::bind(&EventCenter::mouseMove, this, std::placeholders::_1));
-        ci::app::getWindow()->getSignalMouseDrag().connect(std::bind(&EventCenter::mouseDrag, this, std::placeholders::_1));
-        ci::app::getWindow()->getSignalMouseUp().connect(std::bind(&EventCenter::mouseUp, this, std::placeholders::_1));
-        ci::app::getWindow()->getSignalMouseWheel().connect(std::bind(&EventCenter::mouseWheel, this, std::placeholders::_1));
-		
-        //	Connect touch events
-        ci::app::getWindow()->getSignalTouchesBegan().connect(std::bind(&EventCenter::touchesBegan, this, std::placeholders::_1));
-        ci::app::getWindow()->getSignalTouchesMoved().connect(std::bind(&EventCenter::touchesMoved, this, std::placeholders::_1));
-        ci::app::getWindow()->getSignalTouchesEnded().connect(std::bind(&EventCenter::touchesEnded, this, std::placeholders::_1));
-	}
-	
-	//	Process all the event queues for this scene
-	void EventCenter::processEvents(std::vector<NodeRef> nodes)
-	{
-		//	Sort nodes to be top down
-		std::sort(nodes.begin(), nodes.end(),
-			[&nodes] (const NodeRef &a, const NodeRef &b) {
-			  return a->getDrawOrder() > b->getDrawOrder();
-			}
-		);
-		
-		//	Process them
-		mMouseProcessor.processEvents(nodes);
-		mTouchProcessor.processEvents(nodes);
-	}
-		
-} } //  namespace po::scene
+EventCenterRef EventCenter::create() {
+	return EventCenterRef(new EventCenter());
+}
+
+EventCenter::EventCenter() {
+	//	Connect mouse events
+	ci::app::getWindow()->getSignalMouseDown().connect(std::bind(&EventCenter::mouseDown, this, std::placeholders::_1));
+	ci::app::getWindow()->getSignalMouseMove().connect(std::bind(&EventCenter::mouseMove, this, std::placeholders::_1));
+	ci::app::getWindow()->getSignalMouseDrag().connect(std::bind(&EventCenter::mouseDrag, this, std::placeholders::_1));
+	ci::app::getWindow()->getSignalMouseUp().connect(std::bind(&EventCenter::mouseUp, this, std::placeholders::_1));
+	ci::app::getWindow()->getSignalMouseWheel().connect(std::bind(&EventCenter::mouseWheel, this, std::placeholders::_1));
+
+	//	Connect touch events
+	ci::app::getWindow()->getSignalTouchesBegan().connect(std::bind(&EventCenter::touchesBegan, this, std::placeholders::_1));
+	ci::app::getWindow()->getSignalTouchesMoved().connect(std::bind(&EventCenter::touchesMoved, this, std::placeholders::_1));
+	ci::app::getWindow()->getSignalTouchesEnded().connect(std::bind(&EventCenter::touchesEnded, this, std::placeholders::_1));
+}
+
+//	Process all the event queues for this scene
+void EventCenter::processEvents(std::vector<NodeRef> nodes) {
+	//	Sort nodes to be top down
+	std::sort(nodes.begin(), nodes.end(), [&nodes](const NodeRef &a, const NodeRef &b) { return a->getDrawOrder() > b->getDrawOrder(); });
+
+	//	Process them
+	mMouseProcessor.processEvents(nodes);
+	mTouchProcessor.processEvents(nodes);
+}
+}
+} //  namespace po::scene
--- src/poScene/poImage.h
+++ src/poScene/poImage.h
@@ -1,21 +1,21 @@
 /*
  Copyright (c) 2015, Potion Design LLC
  All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
- 
+
  * Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
- 
+
  * Redistributions in binary form must reproduce the above copyright notice,
  this list of conditions and the following disclaimer in the documentation
  and/or other materials provided with the distribution.
- 
+
  * Neither the name of copyright holder nor the names of its
  contributors may be used to endorse or promote products derived from
  this software without specific prior written permission.
- 
+
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -30,45 +30,47 @@
 
 #pragma once
 
-#include "poNode.h"
 #include "cinder/gl/draw.h"
-namespace po { namespace scene {
-	// Images are simple wrappers that draw a ci::gl::Texture to a
-    // quad at their actual size.
-    // The Node attributes (i.e. position, scale, rotation, animation,  etc.) can then be applied
-    //
-    // For more complex usage of textures (i.e. mapping to complex shapes)
-    // use the Shape class.
-    
-    class Image;
-    typedef std::shared_ptr<Image> ImageRef;
-    
-    class Image
-    : public Node
-    {
-    public:
-        //! Create an empty image (texture is a null_ptr, won't draw until set)
-        static ImageRef create();
-        //! Create an image with a ci::gl::TextureRef
-        static ImageRef create(ci::gl::TextureRef texture);
-        
-        //! Draw the texture if set
-        void draw();
-        
-        //! Set the ci::gl::TextureRef
-        void setTexture(ci::gl::TextureRef texture) { mTexture = texture; }
-        //! Get the ci::gl::TextureRef
-        ci::gl::TextureRef getTexture() { return mTexture; }
-        
-        //! Get the bounds of the Image
-        ci::Rectf getBounds();
-        
-    protected:
-        Image(ci::gl::TextureRef texture);
-        
-    private:
-        ci::gl::TextureRef mTexture;
-		
-    };
+#include "poNode.h"
+namespace po {
+namespace scene {
+// Images are simple wrappers that draw a ci::gl::Texture to a
+// quad at their actual size.
+// The Node attributes (i.e. position, scale, rotation, animation,  etc.) can then be applied
+//
+// For more complex usage of textures (i.e. mapping to complex shapes)
+// use the Shape class.
 
-} } //  namespace po::scene
+class Image;
+typedef std::shared_ptr<Image> ImageRef;
+
+class Image : public Node {
+public:
+	//! Create an empty image (texture is a null_ptr, won't draw until set)
+	static ImageRef create();
+	//! Create an image with a ci::gl::TextureRef
+	static ImageRef create(ci::gl::TextureRef texture);
+
+	//! Draw the texture if set
+	void draw();
+
+	//! Set the ci::gl::TextureRef
+	void setTexture(ci::gl::TextureRef texture) {
+		mTexture = texture;
+	}
+	//! Get the ci::gl::TextureRef
+	ci::gl::TextureRef getTexture() {
+		return mTexture;
+	}
+
+	//! Get the bounds of the Image
+	ci::Rectf getBounds();
+
+protected:
+	Image(ci::gl::TextureRef texture);
+
+private:
+	ci::gl::TextureRef mTexture;
+};
+}
+} //  namespace po::scene
